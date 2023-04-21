06c86abe38cdfa87d5b9029a1c90fb91c74a8ed5
ouertaniaziz <mohamedaziz.ouertani@esprit.tn>
2023-04-21 02:30:10 +0100

    integrate doctor files

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/user-routes");
-const donationroute= require("./routes/Donor-routes")
-const doctorRouter = require("./routes/doctor-routes");
+const donationroute = require("./routes/Donor-routes");
 
 const cors = require("cors");
 const dotenv = require("dotenv");
-const prescriptionrouter= require('./routes/prescription-routes');
-const patientrouter = require('./routes/patient-routes');
-const doctorrouter = require('./routes/doctor-routers'); 
-const appointementrouter = require('./routes/appointement-routes');
-const postrouter = require('./routes/post-routes');
-const doctor = require('./routes/doctor-routers');
+const prescriptionrouter = require("./routes/prescription-routes");
+const patientrouter = require("./routes/patient-routes");
+const doctorrouter = require("./routes/doctor-routers");
+const appointementrouter = require("./routes/appointement-routes");
+const postrouter = require("./routes/post-routes");
+const doctor = require("./routes/doctor-routers");
 
 var bodyParser = require("body-parser");
 
@@ -27,7 +25,6 @@ app.use(express.json());
 app.use("/api", router);
 app.use("/api", donationroute);
 
-app.use("/api/doctor", doctorRouter);
 app.use("/api", prescriptionrouter);
 app.use("/api", patientrouter);
 app.use("/api", doctorrouter);
--- routes/doctor-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/doctor-controller");
-const { verifyToken } = require("../middleware/authjwt");
-
-router.post("/profile", controller.getDoctor);
-module.exports = router;
