0b58533168afe68011c670f61be6964a528f405d
malek zlitni <malek.zlitni@esprit.tn>
2023-05-11 05:25:59 +0100

    updates ocr

--- model/Traitement.js
+++ model/Traitement.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const traitementSchema = new mongoose.Schema({
   medicationName: {
     type: String,
-    required: true,
   },
   manufacturer: {
     type: String,
